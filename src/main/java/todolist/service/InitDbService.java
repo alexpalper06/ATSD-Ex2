befa54f33a84c56531d9e6ac699977b808adc54c
alexpalper06 <alex.palacios@goumh.umh.es>
2026-03-10 17:36:50 +0100

    Added my username

--- src/main/java/todolist/service/InitDbService.java
+++ src/main/java/todolist/service/InitDbService.java
@@ -24,6 +24,10 @@ public class InitDbService {
     // para inicializar la base de datos
     @PostConstruct
     public void initDatabase() {
+        /*
+        This should never be done. The information must never be hardcoded, it should instead be stored on a safe
+        file, and should never be uploaded into the repository.
+         */
         Usuario usuario = new Usuario("richard@umh.es");
         usuario.setNombre("Richard Stallman");
         usuario.setPassword("1234");
@@ -34,6 +38,16 @@ public class InitDbService {
 
         Tarea tarea2 = new Tarea(usuario, "Buy milk, cereals and coffee");
         tareaRepository.save(tarea2);
+
+        Usuario userMe = new Usuario("alex.palacios@goumh.umh.es");
+        userMe.setNombre("Alex Palacios Perez");
+
+        userMe.setPassword("1234abc");
+        usuarioRepository.save(userMe);
+
+        Tarea taskMe = new Tarea(userMe, "Create my own username");
+        tareaRepository.save(taskMe);
+
     }
 
 }
